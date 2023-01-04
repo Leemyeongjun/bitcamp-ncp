2a3a1f7a0d89b8f3eca5dba58e3304478a130258
Leemyeongjun <2myeongjun@naver.com>
2023-01-04 09:50:47 +0900

    자바 stack heap methodArea 설명 수업

--- eomcs-java-lang/app/src/main/java/com/eomcs/lang/ex07/Exam0110.java
+++ eomcs-java-lang/app/src/main/java/com/eomcs/lang/ex07/Exam0110.java
@@ -7,13 +7,12 @@ import java.util.Scanner;
 public class Exam0110 {
   public static void main(String[] args) {
     Scanner keyScan = new Scanner(System.in);
-    System.out.print("밑변의 길이? ");
+    System.out.print("밑변의 길이?");
     int len = keyScan.nextInt();
     keyScan.close();
 
     int starLen = 1;
     while (starLen <= len) {
-      // 별 앞에 공백 출력
       int spaceCnt = 1;
       int spaceLen = (len - starLen) / 2;
       while (spaceCnt <= spaceLen) {
@@ -21,14 +20,12 @@ public class Exam0110 {
         spaceCnt++;
       }
 
-      // 별 출력
       int starCnt = 1;
       while (starCnt <= starLen) {
         System.out.print("*");
         starCnt++;
       }
 
-      // 출력 줄 바꾸기
       System.out.println();
       starLen += 2;
     }
