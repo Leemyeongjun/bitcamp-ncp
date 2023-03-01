0f6d7e1c106434abd010d0ec2f6a33655875e212
Leemyeongjun <2myeongjun@naver.com>
2023-03-01 19:34:06 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/myapp/controller/DownloadController.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/controller/DownloadController.java
@@ -1,43 +1,37 @@
-package bitcamp.myapp.servlet;
+package bitcamp.myapp.controller;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import bitcamp.myapp.service.BoardService;
 import bitcamp.myapp.vo.BoardFile;
 
-@WebServlet("/download/boardfile")
-public class DownloadServlet extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+@Controller
+public class DownloadController {
 
-  private BoardService boardService;
+  @Autowired private BoardService boardService;
 
-  @Override
-  public void init() {
-    boardService = (BoardService) getServletContext().getAttribute("boardService");
-  }
-
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+  @RequestMapping("/download/boardfile")
+  public String execute(
+      @RequestParam("fileNo") int fileNo,
+      HttpServletRequest request,
+      HttpServletResponse response) {
 
     try {
-      int fileNo = Integer.parseInt(request.getParameter("fileNo"));
-
       BoardFile boardFile = boardService.getFile(fileNo);
       if (boardFile == null) {
         throw new RuntimeException("파일 정보 없음!");
       }
 
       File downloadFile = new File(
-          this.getServletContext().getRealPath("/board/upload/" + boardFile.getFilepath()));
+          request.getServletContext().getRealPath("/board/upload/" + boardFile.getFilepath()));
       if (!downloadFile.exists()) {
         throw new RuntimeException("파일이 존재하지 않음!");
       }
@@ -59,9 +53,10 @@ public class DownloadServlet extends HttpServlet {
       }
 
     } catch (Exception e) {
-      request.getRequestDispatcher("/downloadfail.jsp").forward(request, response);
       e.printStackTrace();
+      return "/downloadfail.jsp";
     }
+    return null;
   }
 }
 
