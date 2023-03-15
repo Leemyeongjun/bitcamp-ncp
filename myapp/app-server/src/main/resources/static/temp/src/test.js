fbfa4fd1c4b63e98e4aec7c2de32ee88c8941fef
Leemyeongjun <2myeongjun@naver.com>
2023-03-15 16:15:31 +0900

    .

--- myapp/app-server/src/main/resources/static/temp/src/test.js
+++ myapp/app-server/src/main/resources/static/temp/src/test.js
@@ -1,30 +1,34 @@
 "use strict";
-console.log("시이작");
 
-// 좋아요 버튼 리액트 컴퍼넌트 정의
-class LikeButton extends React.Component {
+// 비제어 컴포넌트 - 파일 입력 태그
+// => <input type="file" />은 항상 비제어 컴포넌트다.
+// => 프로그래밍으로 값을 다룰 수 없다.
+//
+
+class FileInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      liked: false,
-    };
+    this.fileInput = React.createRef();
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  // 부모 컴포넌트에서 순차적으로 내려오면서 호출됨
-  // 하는 일은 HTML 태그를 생성하는 일을 한다
-  // 또는 하위 컴포넌트를 실행하는 일을 한다
-  render() {
-    if (this.state.liked) {
-      return "좋아합니다";
-    }
+  handleSubmit(event) {
+    event.preventDefault();
+    alert(`Selected file:  ${this.fileInput.current.files[0].name}`);
+  }
 
+  render() {
     return (
-      <button onClick={() => this.setState({ liked: true })}>좋아요</button>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Upload file:
+          <input type="file" ref={this.fileInput} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
-document.querySelectorAll(".like-container").forEach((likeContainer) => {
-  const root = ReactDOM.createRoot(likeContainer);
-  root.render(React.createElement(LikeButton));
-});
-console.log("조옹료");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<FileInput />);
